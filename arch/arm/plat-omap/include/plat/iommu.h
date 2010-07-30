c5c07af78e2defaf52332d5fc46c8da8ed7b7fec
Hari Kanigeri <h-kanigeri2@ti.com>
2010-07-30 11:15:04 -0500

    omap: iommu: adapt to hwmod implementation
    This patch adapts iommu to hwmod implementation for OMAP4.
    Work needs to be done for OMAP3.
    
    Signed-off-by: Hari Kanigeri <h-kanigeri2@ti.com>
    Signed-off-by: Suman Anna <s-anna@ti.com>

--- arch/arm/plat-omap/include/plat/iommu.h
+++ arch/arm/plat-omap/include/plat/iommu.h
@@ -28,7 +28,6 @@ struct iotlb_entry {
 struct omap_iommu {
 	const char	*name;
 	struct module	*owner;
-	struct clk	*clk;
 	void __iomem	*regbase;
 	struct device	*dev;
 	void		*isr_priv;
@@ -52,6 +51,7 @@ struct omap_iommu {
 	void *ctx; /* iommu context: registres saved area */
 	u32 da_start;
 	u32 da_end;
+	struct platform_device *pdev;
 };
 
 struct cr_regs {
@@ -105,10 +105,12 @@ struct iommu_functions {
 
 struct iommu_platform_data {
 	const char *name;
-	const char *clk_name;
+	const char *oh_name;
 	const int nr_tlb_entries;
 	u32 da_start;
 	u32 da_end;
+	int irq;
+	void __iomem *io_base;
 };
 
 /**
