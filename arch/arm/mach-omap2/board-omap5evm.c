fb70585fee47240ba309549f6775952046b7968f
Andy Green <andy.green@linaro.org>
2012-03-03 06:42:23 +0800

    uEVM edid fixes
    Signed-off-by: Andy Green <andy.green@linaro.org>

--- arch/arm/mach-omap2/board-omap5evm.c
+++ arch/arm/mach-omap2/board-omap5evm.c
@@ -31,6 +31,12 @@
 #endif
 #include <linux/i2c/smsc.h>
 #include <linux/wl12xx.h>
+#include <linux/i2c/twl.h>
+#include <linux/regulator/machine.h>
+#include <linux/regulator/fixed.h>
+#include <linux/mfd/twl6040.h>
+
+#include <drm/drm_edid.h>
 
 #include <asm/mach-types.h>
 #include <asm/mach/arch.h>
@@ -1398,7 +1404,7 @@ static struct omap_dss_device *omap5evm_dss_devices[] = {
 static struct omap_dss_board_info omap5evm_dss_data = {
 	.num_devices	= ARRAY_SIZE(omap5evm_dss_devices),
 	.devices	= omap5evm_dss_devices,
-	.default_device	= &omap5evm_lcd_device,
+	.default_device	= &omap5evm_hdmi_device,
 };
 
 static void __init omap54xx_common_init(void)
@@ -1565,9 +1571,6 @@ static void __init omap_5432_uevm_init(void)
 	usbhs_bdata.reset_gpio_port[2] = GPIO_ETH_NRESET_UEVM;
 
 	omap_mux_init_array(omap5432_uevm_mux, ARRAY_SIZE(omap5432_uevm_mux));
-
-	omap_mux_init_signal("gpio_194", OMAP_PIN_INPUT_PULLUP | OMAP_PIN_OFF_NONE | OMAP_MUX_MODE6);
-	omap_mux_init_signal("gpio_195", OMAP_PIN_INPUT_PULLUP | OMAP_PIN_OFF_NONE | OMAP_MUX_MODE6);
 
 	omap54xx_common_init();
 }
