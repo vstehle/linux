cce019502506e80b03aa911976a249eb229a7e66
Andy Green <andy.green@linaro.org>
2012-03-02 15:40:06 +0800

    uEVM edid fixes
    Signed-off-by: Andy Green <andy.green@linaro.org>

--- arch/arm/mach-omap2/board-omap5evm.c
+++ arch/arm/mach-omap2/board-omap5evm.c
@@ -34,6 +34,8 @@
 #include <linux/regulator/fixed.h>
 #include <linux/mfd/twl6040.h>
 
+#include <drm/drm_edid.h>
+
 #include <asm/mach-types.h>
 #include <asm/mach/arch.h>
 #include <asm/mach/map.h>
@@ -831,7 +833,7 @@ static struct platform_device omap5sevm_abe_audio = {
  */
 static struct i2c_board_info __initdata hdmi_i2c_eeprom[] = {
 	{
-		I2C_BOARD_INFO("eeprom", 0x50),
+		I2C_BOARD_INFO("eeprom", DDC_ADDR),
 	},
 };
 
@@ -848,12 +850,6 @@ static struct platform_device hdmi_edid_device = {
 	.id			= -1,
 	.dev.platform_data	= &i2c_gpio_pdata,
 };
-
-void __init omap5_add_device_i2c(struct i2c_board_info *devices, int nr_devices)
-{
-	i2c_register_board_info(0, devices, nr_devices);
-	platform_device_register(&hdmi_edid_device);
-}
 
 static struct i2c_board_info __initdata omap5evm_i2c_1_boardinfo[] = {
 #ifdef CONFIG_OMAP5_SEVM_PALMAS
@@ -1346,7 +1342,7 @@ static struct omap_dss_device *omap5evm_dss_devices[] = {
 static struct omap_dss_board_info omap5evm_dss_data = {
 	.num_devices	= ARRAY_SIZE(omap5evm_dss_devices),
 	.devices	= omap5evm_dss_devices,
-	.default_device	= &omap5evm_lcd_device,
+	.default_device	= &omap5evm_hdmi_device,
 };
 
 static void __init omap54xx_common_init(void)
@@ -1394,8 +1390,12 @@ static void __init omap_5432_uevm_init(void)
 	omap_mux_init_signal("gpio_80", OMAP_PIN_OUTPUT | OMAP_PIN_OFF_NONE);
 	omap_mux_init_signal("gpio_15", OMAP_PIN_OUTPUT | OMAP_PIN_OFF_NONE);
 
-	omap_mux_init_signal("gpio_194", OMAP_PIN_INPUT_PULLUP | OMAP_PIN_OFF_NONE | OMAP_MUX_MODE6);
-	omap_mux_init_signal("gpio_195", OMAP_PIN_INPUT_PULLUP | OMAP_PIN_OFF_NONE | OMAP_MUX_MODE6);
+	omap_mux_init_signal("gpio_194", OMAP_PIN_INPUT_PULLUP | OMAP_PIN_OFF_NONE);
+	omap_mux_init_signal("gpio_195", OMAP_PIN_INPUT_PULLUP | OMAP_PIN_OFF_NONE);
+
+	/* AUXCLK1 at 19.2kHz for Usb Host chip */
+        omap_writel(0x01000100, 0x4ae0a314);    
+	omap_writel(0, 0x4AE0C858);
 
 	omap54xx_common_init();
 }
