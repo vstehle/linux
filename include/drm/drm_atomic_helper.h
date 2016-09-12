7de8e510ead84a3aab702b74beaeb1f5970b7ff7
Gustavo Padovan <gustavo.padovan@collabora.co.uk>
2016-09-12 16:08:11 -0300

    BACKPORT: drm/fence: allow fence waiting to be interrupted by userspace
    If userspace is running an synchronously atomic commit and interrupts the
    atomic operation during fence_wait() it will hang until the timer expires,
    so here we change the wait to be interruptible so it stop immediately when
    userspace wants to quit.
    
    Also adds the necessary error checking for fence_wait().
    
    v2: Comment by Daniel Vetter
    	- Add error checking for fence_wait()
    
    v3: Rebase on top of new atomic noblocking support
    
    v4: Comment by Maarten Lankhorst
    	- remove 'swapped' bitfield as it was duplicating information
    
    v5: Comments by Maarten Lankhorst
    	- assign plane->state to plane_state if !intr
    	- squash previous patch into this one
    
    v6: Comment by Sean Paul
    	- rename intr to pre_swap
    
    Signed-off-by: Gustavo Padovan <gustavo.padovan@collabora.co.uk>
    Reviewed-by: Maarten Lankhorst <maarten.lankhorst@linux.intel.com>
    [seanpaul fixed a couple checkpatch warnings and moved the preswap comment]
    Signed-off-by: Sean Paul <seanpaul@chromium.org>
    Link: http://patchwork.freedesktop.org/patch/msgid/1473707291-14781-1-git-send-email-gustavo@padovan.org
    
    (cherry picked from commit f6ce410a59a48aff47bb7e18ab40497e4e80d275)
    from topic/drm-misc-2016-09-19
    Signed-off-by: Tarun <tarun.vyas@intel.com>
    
    BUG=b:36446508
    TEST=No regressions in KBL and APL based devices
    
     Conflicts: [Tarun: Ctx delta. Just retain the HEAD]
    	drivers/gpu/drm/msm/msm_atomic.c
    
    Change-Id: I7e12a6c7258e9f746c8582a1e7f739572d9b4894
    Reviewed-on: https://chromium-review.googlesource.com/460662
    Commit-Ready: Sean Paul <seanpaul@chromium.org>
    Tested-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Sean Paul <seanpaul@chromium.org>

--- include/drm/drm_atomic_helper.h
+++ include/drm/drm_atomic_helper.h
@@ -45,6 +45,9 @@ int drm_atomic_helper_commit(struct drm_device *dev,
 			     struct drm_atomic_state *state,
 			     bool nonblock);
 
+int drm_atomic_helper_wait_for_fences(struct drm_device *dev,
+					struct drm_atomic_state *state,
+					bool pre_swap);
 bool drm_atomic_helper_framebuffer_changed(struct drm_device *dev,
 					   struct drm_atomic_state *old_state,
 					   struct drm_crtc *crtc);
