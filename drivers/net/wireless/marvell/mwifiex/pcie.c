5f5ea5c5e8dbeb0f419bcc06c2a109fa2ddd1689
Xinming Hu <huxm@marvell.com>
2016-10-06 23:36:24 +0530

    CHROMIUM: mwifiex: reset card->adapter during device unregister
    card->adapter gets initialized in mwifiex_register_dev(). As it's not
    cleared in mwifiex_unregister_dev(), we may end up accessing the memory
    which is already free in below scenario.
    
    Scenario: Driver initialization is failed due to incorrect firmware or
    some other reason. Meanwhile device reboot/unload occurs.
    
    This is safe, now that we've properly synchronized suspend() and
    remove() with the FW initialization thread; now that code can simply
    check for 'card->adapter == NULL' and exit safely.
    
    BUG=chrome-os-partner:58129, chrome-os-partner:58291
    TEST=suspend/reboot tests
    
    Change-Id: Ica39c3dd678eccea0c8b953f8eaf5ed700f6a047
    Signed-off-by: Xinming Hu <huxm@marvell.com>
    Signed-off-by: Amitkumar Karwar <akarwar@marvell.com>
    Signed-off-by: Brian Norris <briannorris@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/403534
    Reviewed-by: Dmitry Torokhov <dtor@chromium.org>

--- drivers/net/wireless/marvell/mwifiex/pcie.c
+++ drivers/net/wireless/marvell/mwifiex/pcie.c
@@ -3036,6 +3036,7 @@ static void mwifiex_unregister_dev(struct mwifiex_adapter *adapter)
 		mwifiex_pcie_delete_rxbd_ring(adapter);
 		mwifiex_pcie_delete_txbd_ring(adapter);
 		card->cmdrsp_buf = NULL;
+		card->adapter = NULL;
 	}
 }
 
