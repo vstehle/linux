f5069c8353be93e38340c695e751aa4b48f18509
Haim Dreyfuss <haim.dreyfuss@intel.com>
2016-12-12 14:09:49 +0200

    CHROMIUM: iwl7000: pcie: re-configure IVAR table after suspend-resume
    During the suspend/resume flow some HW blocks are reset.  This causes
    the IVAR table to be completely erased.  This table is where interrupt
    causes are bound to specific IRQs.  When the table is empty the
    interrupt handlers are not called correctly.  Fix this by reconfiguring
    the IVAR table after resume.
    
    Signed-off-by: Haim Dreyfuss <haim.dreyfuss@intel.com>
    iwl7000-tree: e5e01705dfea53ee3ceeba5364210b02083ec270

--- drivers/net/wireless/iwl7000/iwlwifi/pcie/trans.c
+++ drivers/net/wireless/iwl7000/iwlwifi/pcie/trans.c
@@ -1355,13 +1355,19 @@ static void iwl_pcie_conf_msix_hw(struct iwl_trans_pcie *trans_pcie)
 	struct iwl_trans *trans = trans_pcie->trans;
 
 	if (!trans_pcie->msix_enabled) {
-		if (trans->cfg->mq_rx_supported)
+		if (trans->cfg->mq_rx_supported &&
+		    test_bit(STATUS_DEVICE_ENABLED, &trans->status))
 			iwl_write_prph(trans, UREG_CHICK,
 				       UREG_CHICK_MSI_ENABLE);
 		return;
 	}
-
-	iwl_write_prph(trans, UREG_CHICK, UREG_CHICK_MSIX_ENABLE);
+	/*
+	 * The IVAR table needs to be configured again after reset,
+	 * but if the device is disabled, we can't write to
+	 * prph.
+	 */
+	if (test_bit(STATUS_DEVICE_ENABLED, &trans->status))
+		iwl_write_prph(trans, UREG_CHICK, UREG_CHICK_MSIX_ENABLE);
 
 	/*
 	 * Each cause from the causes list above and the RX causes is
